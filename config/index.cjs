fd38e71dc60984adb0c634dbdfe7da9799725a9a
106461 <107380938+106461@users.noreply.github.com>
2022-09-26 11:56:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -81,7 +81,7 @@ const USER_CONFIG = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
+      {
           type: '*生日', name: '红', year: '2003', date: '10-12',
         },
         {
@@ -89,10 +89,10 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '你的好兄弟也就是我的生日', year: '2002', date: '08-13',
         },
         {
-          type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
+          type: '节日', name: '说要给我补生日的日子', year: '2022', date: '09-08',
         }
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
